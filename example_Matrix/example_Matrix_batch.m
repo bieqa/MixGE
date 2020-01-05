8e7b58bdd6ad7599e0dd45730e8edaff98dbb56f
Anqi Qiu <58940596+bieqa@users.noreply.github.com>
2020-01-05 20:05:13 +0800

    Add files via upload

---
+++ example_Matrix/example_Matrix_batch.m
@@ -0,0 +1,8 @@
+Data = {[pwd,'/example_Matrix_data.mat']};
+Matrix = 'Matrix';
+Genotype = 'Genotype';
+Characteristic = '';
+Weight = '';
+Covariate = 'Covariate';
+Environment = 'Environment';
+Output = pwd;
